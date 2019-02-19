1f4984bb51ef848dd5adb30980f508170d85b3d3
Alchemist <alex@alchemist.codes>
2019-02-18 19:02:08 -0800

    Triangle - take 2
    Rough first pass

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -16,14 +16,26 @@
 # and
 #   about_triangle_project_2.py
 #
+# Helpful snippet to drop into interactive session (like Ruby's binding.pry)
+# import code; code.interact(local=dict(globals(), **locals()))
+# from https://gist.github.com/obfusk/208597ccc64bf9b436ed
+#
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
+    validate_triangle_sides(a, b, c)
     if a == b == c:
         return "equilateral"
     elif a == b or a == c or b == c:
         return "isosceles"
     else:
         return "scalene"
+
+def validate_triangle_sides(*args):
+    ordered = sorted(args)
+    if ordered[0] + ordered[1] < ordered[2]:
+        raise TriangleError("Sum of two sides should be greater than the third")
+
+    if ordered[0] <= 0 or ordered[1] <= 0 or ordered[2] <= 0:
+        raise TriangleError("All sides must be greater than 0")
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
